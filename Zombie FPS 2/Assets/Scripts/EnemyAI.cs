355e401eb9b005404ed7a68339a3cf283bd497f2
stachowiakw@gmail.com <stachowiakw@gmail.com>
2020-10-08 00:50:52 +0200

    Lecture 172

---
+++ Zombie FPS 2/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float chaseRange = 5;
+
+    NavMeshAgent navMeshAgent;
+    float distanceToTarget = Mathf.Infinity;
+    
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        distanceToTarget = Vector3.Distance(target.position, transform.position);
+        if (distanceToTarget <= chaseRange)
+        { navMeshAgent.SetDestination(target.position); }
+        else
+        { navMeshAgent.SetDestination(transform.position); }
+    }
+}
