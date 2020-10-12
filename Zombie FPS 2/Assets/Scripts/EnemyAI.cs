98a4d1bc953f7678f077f1ad4356e9e2f10e83d2
stachowiakw@gmail.com <stachowiakw@gmail.com>
2020-10-13 01:03:44 +0200

    Lecture 174

--- Zombie FPS 2/Assets/Scripts/EnemyAI.cs
+++ Zombie FPS 2/Assets/Scripts/EnemyAI.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
@@ -6,10 +7,11 @@ using UnityEngine.AI;
 public class EnemyAI : MonoBehaviour
 {
     [SerializeField] Transform target;
-    [SerializeField] float chaseRange = 5;
+    [SerializeField] float chaseRange = 5f;
 
     NavMeshAgent navMeshAgent;
     float distanceToTarget = Mathf.Infinity;
+    bool isProvoked = false;
     
     void Start()
     {
@@ -19,9 +21,37 @@ public class EnemyAI : MonoBehaviour
     void Update()
     {
         distanceToTarget = Vector3.Distance(target.position, transform.position);
-        if (distanceToTarget <= chaseRange)
-        { navMeshAgent.SetDestination(target.position); }
-        else
-        { navMeshAgent.SetDestination(transform.position); }
+        if (isProvoked)
+        { EngageTarget(); }
+        else if (distanceToTarget <= chaseRange)
+        { isProvoked = true; }
+        //    else
+        //    { navMeshAgent.SetDestination(transform.position); }
+        //
+    }
+
+    private void EngageTarget()
+    {
+        if (distanceToTarget > navMeshAgent.stoppingDistance)
+        { ChaseTarget(); }
+
+        if (distanceToTarget <= navMeshAgent.stoppingDistance)
+        { AttackTarget(); }
+    }
+
+    private void ChaseTarget()
+    {
+        navMeshAgent.SetDestination(target.position);
+    }
+
+    private void AttackTarget()
+    {
+        Debug.Log(name + " has seeked and is detroyin " + target.name);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = new Color(1, 1, 0, 0.75F);
+        Gizmos.DrawSphere(transform.position, chaseRange);
     }
 }
